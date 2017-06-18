1fdbd0a1d708f24f192a1ee1cb2f2e51a73323a4
XDean <373216024@qq.com>
2017-06-18 10:20:45 +0800

    Q44605815

--- src/main/java/xdean/stackoverflow/rx/Q44605815.java
+++ src/main/java/xdean/stackoverflow/rx/Q44605815.java
@@ -3,6 +3,7 @@ package xdean.stackoverflow.rx;
 import io.reactivex.Observable;
 
 public class Q44605815 {
+  @SuppressWarnings("null")
   public static void main(String[] args) {
     Observable<Observable<Integer>> o = null;
     o.flatMap(inner -> inner.take(1).map(i -> inner));
