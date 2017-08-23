d3b43067dd1a5a4a0acc07bcfdb4aaf3f3ca4a1f
XDean <373216024@qq.com>
2017-08-24 07:44:00 +0800

    update

--- src/main/java/xdean/stackoverflow/rx/Q44004014.java
+++ src/main/java/xdean/stackoverflow/rx/Q44004014.java
@@ -2,13 +2,13 @@ package xdean.stackoverflow.rx;
 
 import rx.Observable;
 import rx.schedulers.Schedulers;
-import xdean.jex.util.task.TaskUtil;
+import xdean.jex.util.lang.ExceptionUtil;
 
 public class Q44004014 {
   public static void main(String[] args) throws InterruptedException {
     Observable.just("a", "b", "c")
         .zipWith(Observable.range(1, 100).observeOn(Schedulers.io()).doOnNext(System.err::println), (a, c) -> c)
-        .doOnNext(i -> TaskUtil.uncheck(() -> Thread.sleep(500)))
+        .doOnNext(i -> ExceptionUtil.uncheck(() -> Thread.sleep(500)))
         .forEach(System.out::println);
     Thread.sleep(1000);
   }
