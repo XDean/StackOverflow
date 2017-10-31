e098e431285c48ec2e30bfd6c02229854bc40e17
XDean <373216024@qq.com>
2017-10-31 15:00:57 +0800

    modify

--- src/main/java/xdean/stackoverflow/java/reflection/Q46844275.java
+++ src/main/java/xdean/stackoverflow/java/reflection/Q46844275.java
@@ -1,10 +1,14 @@
 package xdean.stackoverflow.java.reflection;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 
 import sun.misc.Cleaner;
+import sun.misc.Unsafe;
 import xdean.jex.util.lang.FinalizeSupport;
+import xdean.jex.util.lang.UnsafeUtil;
 
 public class Q46844275 {
   public static void main(String[] args) throws Exception {
@@ -12,28 +16,55 @@ public class Q46844275 {
     byFinalizeSupport();
   }
 
-  private static void byFinalizeSupport() throws InterruptedException {
-    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(100);
-    FinalizeSupport.finalize(byteBuffer, () -> System.out.println("Clean"));
+  static Constructor<? extends ByteBuffer> constructor;
+  static Field addressField;
+  static Unsafe unsafe;
+  static {
+    try {
+      Class<? extends ByteBuffer> directClass = ByteBuffer.allocateDirect(1).getClass();
+      constructor = directClass.getDeclaredConstructor(long.class, int.class);
+      constructor.setAccessible(true);
+      addressField = Buffer.class.getDeclaredField("address");
+      addressField.setAccessible(true);
+      unsafe = UnsafeUtil.getUnsafe();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static ByteBuffer newDirectByteBuffer(long addr, int cap) throws Exception {
+    // just mock jni newDirectByteBuffer, ignore the address
+    addr = unsafe.allocateMemory(cap);
+    return constructor.newInstance(addr, cap);
+  }
+
+  private static long getAddress(ByteBuffer buffer) throws Exception {
+    return (long) addressField.get(buffer);
+  }
+
+  private static void byFinalizeSupport() throws Exception {
+    ByteBuffer byteBuffer = newDirectByteBuffer(0x0000ffaf, 100);
+    long addr = getAddress(byteBuffer);
+    FinalizeSupport.finalize(byteBuffer, () -> {
+      unsafe.freeMemory(addr);
+      System.out.println("byFinalizeSupport clean");
+    });
     byteBuffer = null;
     System.gc();
     Thread.sleep(10);
   }
 
-  private static void byReflection()
-      throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException, InterruptedException {
-    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(100);
+  private static void byReflection() throws Exception {
+    ByteBuffer byteBuffer = newDirectByteBuffer(0x0000ffaf, 100);
+    long addr = getAddress(byteBuffer);
+    Cleaner cleaner = Cleaner.create(byteBuffer, () -> {
+      unsafe.freeMemory(addr);
+      System.out.println("byReflection clean");
+    });
+    // in fact this three lines is not needed.
     Field cleanerField = Class.forName("java.nio.DirectByteBuffer").getDeclaredField("cleaner");
     cleanerField.setAccessible(true);
-    Cleaner cleaner = (Cleaner) cleanerField.get(byteBuffer);
-    Field thunkField = Cleaner.class.getDeclaredField("thunk");
-    thunkField.setAccessible(true);
-    Runnable originThunk = (Runnable) thunkField.get(cleaner);
-    thunkField.set(cleaner, (Runnable) () -> {
-      System.out.println("To clean");
-      originThunk.run();
-      System.out.println("Cleaned");
-    });
+    cleanerField.set(byteBuffer, cleaner);
     byteBuffer = null;
     System.gc();
     Thread.sleep(10);
